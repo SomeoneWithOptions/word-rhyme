b7928c4fff148001b9d8ff132c77c86050345c64
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-05 20:56:10 -0500

    event litener for rhyme ford results

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,18 +5,29 @@ import Loading from "./Loading";
 function Form() {
   const [APIWords, setAPIWords] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [RhymeWord, setRhymeWord] = useState("");
 
   const wordInput = useRef(null);
 
   const handleClick = (e) => {
-    setLoading(true);
     e.preventDefault();
+    setLoading(true);
     fetchRhymes(wordInput.current.value).then((data) => {
       setAPIWords(data);
       setLoading(false);
     });
   };
 
+
+ function handleRhymeWords(e){ 
+   e.preventDefault();
+   wordInput.current.value = e.target.innerText;
+   fetchRhymes(wordInput.current.value).then((data) => {
+    setAPIWords(data);
+    setLoading(false);
+   });
+   
+  }
   return (
     <>
       <form id="form" onSubmit={e => handleClick(e)}>
@@ -30,7 +41,7 @@ function Form() {
         ) : (
           <ul>
               {APIWords.map((item) => {
-                return <li key={item.word} className="rhyme-words-effect">
+                return <li key={item.word} className="rhyme-words-effect" onClick={e => handleRhymeWords(e)}>
                   {item.word}
                 </li>
               }
